14996381d37b2af3da557db482375d7595449261
Al Francis Gabriel Bolima <alfrancisgabriel@gmail.com>
2020-12-04 19:46:39 +0800

    feat:added a bolierplate of express

---
+++ index.js
@@ -0,0 +1,14 @@
+const crypto = require("crypto");
+const { exec } = require("child_process");
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.listen(PORT, (req, res) => {
+	console.log("server listening on port:" + PORT);
+});
